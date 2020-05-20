9a397c1aca24569860050cf14316830ff55f6d56
Chau Batkunda <maniklusi31@gmail.com>
2020-05-21 02:22:50 +0700

    mengubah model Mpelajaran dan Mwaka

--- app/models/Mpelajaran_model.php
+++ app/models/Mpelajaran_model.php
@@ -5,11 +5,6 @@ class Mpelajaran_model extends CI_Model
 {
     public function getAllMapel()
     {
-        // $this->db->select('*');
-        // $this->db->from('t_nilai');
-        // $this->db->join('t_mapel', 't_mapel.id_mapel = t_nilai.mapel_id');
-        // $this->db->join('t_siswa', 't_siswa.id_siswa = t_nilai.siswa_id');
-        // $this->db->order_by('kode_mapel', 'esc');
         return $this->db->get('t_mapel')->result();
     }
 
--- app/models/Waka_model.php
+++ app/models/Waka_model.php
@@ -9,7 +9,6 @@ class Waka_model extends CI_Model
         $this->db->from('t_waka');
         $this->db->join('t_guru', 't_guru.kode_guru = t_waka.guru_id');
         $this->db->join('t_mapel', 't_mapel.id_mapel = t_waka.mapel_id');
-        // $this->db->order_by('id_waka', 'desc');
         return $this->db->get()->result();
     }
 
