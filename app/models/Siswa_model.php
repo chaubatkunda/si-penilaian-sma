b171d50114b12d86663a81b176cfa70d0736aea1
chaubatkunda <maniklusi31@gmail.com>
2020-07-12 17:20:25 +0700

    hapus user_guide

--- app/models/Siswa_model.php
+++ app/models/Siswa_model.php
@@ -3,15 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Siswa_model extends CI_Model
 {
-
-
-
     public function getAllSiswa()
     {
         // return $this->db->get('t_siswa')->result();
         $this->db->select('*');
         $this->db->from('t_siswa');
-        $this->db->join('t_kelas', 't_kelas.id_kelas = t_siswa.id_kls');
+        $this->db->join('t_kelas', 't_kelas.id_kelas = t_siswa.kelas_id');
         // $this->db->where('t_siswa.id_siswa');
         return $this->db->get()->result();
     }
@@ -19,7 +16,7 @@ class Siswa_model extends CI_Model
     {
         $this->db->select('*');
         $this->db->from('t_siswa');
-        $this->db->join('t_kelas', 't_kelas.id_kelas = t_siswa.id_kls');
+        $this->db->join('t_kelas', 't_kelas.id_kelas = t_siswa.kelas_id');
         $this->db->where('id_siswa', $id);
         return $this->db->get()->row();
         // return $this->db->get_where('t_siswa', ['id_siswa' => $id])->row();
